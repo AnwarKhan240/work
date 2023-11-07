e1319f5d5ca61becdcfce285c89aa87f47cc1ebb
Anwar Khan <94431998+AnwarKhan240@users.noreply.github.com>
2023-11-07 12:01:20 +0530

    Add files via upload

---
+++ src/test/java/com/lbrce/cse2/UserValidationTest.java
@@ -0,0 +1,19 @@
+package com.lbrce.cse2;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class UserValidationTest {
+	@Test
+	public void testCase1()
+	{
+		UserValidation uv=new UserValidation();
+		Assert.assertEquals(false,uv.check("", ""));
+	}
+	@Test
+	public void testCase2()
+	{
+		UserValidation uv=new UserValidation();
+		Assert.assertEquals(true,uv.check("anwar", "anwar@123"));
+	}
+}
